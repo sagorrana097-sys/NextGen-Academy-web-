3c43f3527c707f7cb6d05dba11552f0bc2ed01b6
sagorrana097-sys <sagorrana097@gmail.com>
2026-08-21 21:44:43 +0600

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://nextgen-academy-web.onrender.com/api';
 
 async function request(endpoint, options = {}) {
   const token = localStorage.getItem('nextgen_token');
